954f34afd797d640d040f4ca0aab40a4eb001e79
Lor3m1psum <lorena.rosales.pajaro87@gmail.com>
2024-02-21 19:48:33 -0500

    implementing mobile responsive and final styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 
-import './App.css'
+import { useEffect, useState } from 'react';
 import { CardGrid } from './components/CardGrid/CardGrid';
 import { CardList } from './components/CardList/CardList';
+import Slider from 'react-slick';
+import { useWindowSize } from '@uidotdev/usehooks';
 import { data } from './assets/data.json';
-import { useEffect, useState } from 'react';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './App.css'
 
 function App() {
+  const size = useWindowSize();
   const [celebrityData, setCelebrityData] = useState([]);
   const [cardDisplay, setCardDisplay] = useState("grid");
 
@@ -18,6 +24,12 @@ function App() {
     }
   }, []);
 
+  useEffect(() => {
+    if (size.width < 768 && cardDisplay !== "grid") {
+      setCardDisplay("grid");
+    }
+  }, [size.width, cardDisplay])
+
   const onVoteChanged = ({ name, newVotes }) => {
     const updatedCelebrities = celebrityData.map(celebrity => celebrity.name === name ? {...celebrity, votes: newVotes} : celebrity);
     setCelebrityData(updatedCelebrities);
@@ -25,16 +37,24 @@ function App() {
   }
   
   const ComponentToRender = cardDisplay === "grid" ? CardGrid : CardList;
-
+  const cards = celebrityData.map( (celebrity) =>(<ComponentToRender key={celebrity.name} type={cardDisplay} onVoteChanged={onVoteChanged} imgSrc={celebrity.picture} imgAlt="Celebrity image" name={celebrity.name} description={celebrity.description} votes={celebrity.votes} category={celebrity.category} lastUpdated={celebrity.lastUpdated} />))
+  const settings = {
+    arrows: false,
+    slidesToShow: 1.2,
+    infinite: false,
+  };
   return (
     <div className="App">
-      <div className="select-view">Previous Rulings <select className="select-style" onChange={(event) => setCardDisplay(event.target.value)}>
+      <div className="select-view">Previous Rulings <select value={cardDisplay} className="select-style" onChange={(event) => setCardDisplay(event.target.value)}>
         <option className="select-option" value="grid">Grid</option>
         <option className="select-option" value="list">List</option>
       </select></div>
-      
       <div className={`card-wrapper ${cardDisplay === "grid" ? "card-wrapper-grid" : "card-wrapper-list" }`}>
-        {celebrityData.map( (celebrity) =>(<ComponentToRender key={celebrity.name} type={cardDisplay} onVoteChanged={onVoteChanged} imgSrc={celebrity.picture} imgAlt="Celebrity image" name={celebrity.name} description={celebrity.description} votes={celebrity.votes} category={celebrity.category} lastUpdated={celebrity.lastUpdated} />))}  
+        {size.width < 768 ?
+        <Slider {...settings}>
+          {cards}</Slider> :
+        cards
+        }
       </div>
     </div>
   )
